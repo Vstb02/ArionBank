e29523ceea8f283a79d702bd1c308019e8fe6127
Владислав Стеблев <steblevvlad037@gmail.com>
2022-04-06 20:40:21 +0300

    Добавил контроллер дла карт и удалил мусор

---
+++ src/WebApi/ArionBank.Main/Controllers/ApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArionBank.Main.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]/[Action]")]
+    public class ApiControllerBase : ControllerBase
+    {
+    }
+}
