b14cd0be8dba46c9b48ed937863b1fe985746e8a
Владислав Стеблев <steblevvlad037@gmail.com>
2022-04-06 21:39:52 +0300

    Добавил сервисы для карты и аккаунта

---
+++ scr/Core/ArionBank.Application/Services/CardService.cs
@@ -0,0 +1,13 @@
+﻿using ArionBank.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArionBank.Application.Services
+{
+    public class CardService : ICardService
+    {
+    }
+}
